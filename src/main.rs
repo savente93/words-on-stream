533e8eb3eb196c84f763cbe8e22d846059583066
Sam Vente <savente93@gmail.com>
2023-08-17 22:23:18 +0200

    add cli interface

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ use std::collections::BTreeSet;
 use std::include_str;
 
 type DictType = BTreeSet<String>;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    word: Option<String>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+}
 
 fn main() {
-    let word = "aylwari".as_bytes();
+    let cli = Cli::parse();
+    let word_string = cli.word.unwrap_or(String::from("aylwari"));
+    let word = word_string.as_bytes();
 
     let dict: DictType = BTreeSet::from(
         include_str!("dict2.txt")
@@ -16,10 +29,7 @@ fn main() {
             .collect::<BTreeSet<String>>(),
     );
 
-    let mut words = all_permutations(&word, &dict);
-    words.sort();
-    words.dedup();
-    println!("{:?}", words);
+    let words = all_permutations(&word, &dict, true);
 }
 
 fn power_set(set: Vec<u8>) -> Vec<Vec<u8>> {
@@ -43,7 +53,7 @@ fn power_set(set: Vec<u8>) -> Vec<Vec<u8>> {
     return sets;
 }
 
-fn all_permutations(word: &[u8], dict: &DictType) -> Vec<String> {
+fn all_permutations(word: &[u8], dict: &DictType, print_progress: bool) -> Vec<String> {
     let mut acc: Vec<String> = Vec::new();
     let indices = (0..word.len() as u8).collect::<Vec<u8>>();
     let mut indices_power_set: Vec<Vec<u8>> = power_set(indices);
@@ -51,7 +61,7 @@ fn all_permutations(word: &[u8], dict: &DictType) -> Vec<String> {
         if idx_set.len() == 0 {
             continue;
         }
-        generate_permut(&word, dict, idx_set.clone().len(), idx_set, &mut acc);
+        generate_permut(word, dict, idx_set.len(), idx_set, &mut acc, print_progress);
     }
     return acc;
 }
@@ -68,25 +78,29 @@ fn generate_permut(
     k: usize,
     arr: &mut Vec<u8>,
     acc: &mut Vec<String>,
+    print_progress: bool,
 ) {
     if k == 1 {
         let mut permutation = String::with_capacity(word.len());
         arr.iter()
             .for_each(|i| permutation.push(word[*i as usize] as char));
 
-        if dict.contains(&permutation) {
+        if dict.contains(&permutation) && !acc.contains(&permutation) {
+            if print_progress {
+                println!("{}", permutation);
+            }
             acc.push(permutation);
         }
         return;
     } else {
-        generate_permut(&word, dict, k - 1, arr, acc);
+        generate_permut(&word, dict, k - 1, arr, acc, print_progress);
         for i in 0..(k - 1) {
             if k % 2 == 0 {
                 swap(arr, i, k - 1);
             } else {
                 swap(arr, 0, k - 1);
             }
-            generate_permut(&word, dict, k - 1, arr, acc);
+            generate_permut(&word, dict, k - 1, arr, acc, print_progress);
         }
     }
 }
